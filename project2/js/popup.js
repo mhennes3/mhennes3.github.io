acfe9588bc44240ed097d89b7103524c3395d5f5
Matthew Hennessey <mhennes3@nd.edu>
2019-03-25 12:11:12 -0400

    changes

--- project2/js/popup.js
+++ project2/js/popup.js
@@ -1,17 +1,146 @@
-//************************ Templates ************************
-
-// Button to open a modal
 $(document).ready(function (){
-    //ID or Class name of the button goes into the first section
+
     $('#button1').click(function() {
-        //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#artwork-modal").toggleClass("visible");
+        
+        $("#artwork-modal").toggleClass("visible")
         $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#artwork-close').click(function() {
+        $("#artwork-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
 });
 
-// Button to close a modal
 $(document).ready(function (){
-    $('#button1').click(function() {
-        $("#artwork-modal").toggleClass("visible");
-         $(".fadeout").toggleClass("visible");
+    $('#button2').click(function() {
+        
+        $("#lighting-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#lighting-close').click(function() {
+        $("#lighting-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+    $('#button3').click(function() {
+        
+        $("#seating-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#seating-close').click(function() {
+        $("#seating-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+
+    $('#button4').click(function() {
+        
+        $("#cabinets-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#cabinets-close').click(function() {
+        $("#cabinets-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+    $('#button5').click(function() {
+        
+        $("#counters-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#counters-close').click(function() {
+        $("#counters-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+    $('#button6').click(function() {
+        
+        $("#plants-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#plants-close').click(function() {
+        $("#plants-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+
+    $('#button7').click(function() {
+        
+        $("#bedding-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#bedding-close').click(function() {
+        $("#bedding-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+    $('#button8').click(function() {
+        
+        $("#windows-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#windows-close').click(function() {
+        $("#windows-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
+});
+
+$(document).ready(function (){
+    $('#button9').click(function() {
+        
+        $("#rug-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible");
+         });
+
+});
+    
+$(document).ready(function (){
+    $('#rug-close').click(function() {
+        $("#rug-modal").toggleClass("visible")
+        $(".fadeout").toggleClass("visible")
+        });
 });
